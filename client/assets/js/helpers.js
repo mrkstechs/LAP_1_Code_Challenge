01544cf45c297b592e7138ac7023de57b359ceea
Bradley Gichangi <bradgichangi@gmail.com>
2022-10-19 17:38:13 +0100

    Additions to JavaScript, looping to display results on page

---
+++ client/assets/js/helpers.js
@@ -0,0 +1,55 @@
+let sitesArray = []
+
+class Site {
+    constructor(data) {
+        this.title = data.title,
+        this.url = data.link,
+        this.img = data.pagemap.cse_thumbnail.src
+        this.desc = data.snippet
+    }
+
+    static getAllResults () {
+        return sitesArray.map(obj => new Site(obj))
+    }
+
+    static randomResult () {
+        const allSites = sitesArray.map(obj => new Site(obj))
+        return allQuotes[Math.floor(Math.random() * allQuotes.length)]
+
+    }
+}
+
+let main = document.querySelector('main')
+
+for (let i; i < sitesArray.length; i++){
+    let div = document.createElement('div')
+    div.className = 'site'
+
+    let urlP = document.createElement('p')
+    urlP.className = 'url'
+    let urlA = document.createElement('a')
+    urlA.textContent = sitesArray[i].url
+    urlP.append(urlA)
+
+    let title = document.createElement('h3')
+    let titleA = document.createElement('a')
+    titleA.textContent = sitesArray[i].url
+    title.append(titleA)
+
+    let imgA = document.createElement('p')
+    imgA.setAttribute("href", sitesArray[i].url);
+    let img = document.createElement('img')
+    imgA.setAttribute("src", sitesArray[i].img);
+    imgA.append(img)
+
+    let descP = document.createElement('p')
+    descP.className = 'desc'
+    descP.textContent = sitesArray[i].desc
+
+    div.append(urlP)
+    div.append(title)
+    div.appemd(imgA)
+    div.append(descP)
+    main.append(div)
+}
+
