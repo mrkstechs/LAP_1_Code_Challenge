2501181143a3dfcdbb837357ff227e7db0f28af4
Bradley Gichangi <bradgichangi@gmail.com>
2022-10-20 10:36:49 +0100

    Changes to JS and CSS, working results page

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -1,9 +1,16 @@
 const searchBtn = document.querySelector('#search-btn')
 const luckyBtn = document.querySelector('#lucky-btn')
 
+
 const userQuery = document.querySelector('#search-bar')
 
 searchBtn.addEventListener('click', e => {
+    e.preventDefault()
+    let query = userQuery.value
+    window.location.href = `/results?q=${query}`
+})
+
+luckyBtn.addEventListener('click', e => {
     e.preventDefault()
     let query = userQuery.value
     window.location.href = `/results?q=${query}`
